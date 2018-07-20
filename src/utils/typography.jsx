65da1c42672f2d565d0aa7686595c43fdd6edb6d
Nop Jiarathanakul <prutsdomj@gmail.com>
2018-07-19 22:39:00 -0700

    use kirkham typography

--- src/utils/typography.jsx
+++ src/utils/typography.jsx
@@ -1,15 +1,7 @@
 import Typography from "typography";
-import Wordpress2016 from "typography-theme-wordpress-2016";
+import theme from "typography-theme-kirkham";
 
-Wordpress2016.overrideThemeStyles = () => ({
-  "a.gatsby-resp-image-link": {
-    boxShadow: "none",
-  },
-});
-
-delete Wordpress2016.googleFonts;
-
-const typography = new Typography(Wordpress2016);
+const typography = new Typography(theme);
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== "production") {
