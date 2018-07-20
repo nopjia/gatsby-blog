87203e91805329dd540ffab4d46237a34401372f
Nop Jiarathanakul <prutsdomj@gmail.com>
2018-07-19 18:57:10 -0700

    gatsby new https://github.com/gatsbyjs/gatsby-starter-blog#v2

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true,
+  },
+  "plugins": [
+    "react",
+  ],
+  "parserOptions": {
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "experimentalObjectRestSpread": true,
+      "jsx": true,
+    },
+  }
+}
