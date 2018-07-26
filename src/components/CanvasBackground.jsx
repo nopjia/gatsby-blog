cbd62b09a91b40baba339b5ba322a316b94c61a9
Nop Jiarathanakul <prutsdomj@gmail.com>
2018-07-25 18:15:39 -0700

    organize code CanvasBackground

--- src/components/CanvasBackground.jsx
+++ src/components/CanvasBackground.jsx
@@ -9,16 +9,24 @@ class CanvasBackground extends React.Component {
     this.start = this.start.bind(this);
     this.stop = this.stop.bind(this);
     this.update = this.update.bind(this);
-
-    this.bgColor = 0xffffff;
   }
 
   componentDidMount() {
-    this.container = document.getElementById("canvas-wrap");
-    this.canvas = document.getElementById("canvas");
+    this.init();
+  }
 
-    this.w = this.container.clientWidth;
-    this.h = this.container.clientHeight;
+  componentWillUnmount() {
+    this.stop();
+    this.mount.removeChild(this.renderer.domElement);
+    console.log("CanvasBackground unmount");
+  }
+
+  init() {
+    console.log("CanvasBackground init");
+
+    this.bgColor = 0xffffff;
+    this.w = this.mount.clientWidth;
+    this.h = this.mount.clientHeight;
 
     this.renderer = new THREE.WebGLRenderer({
       canvas: this.canvas,
@@ -27,18 +35,25 @@ class CanvasBackground extends React.Component {
     });
     this.renderer.setSize(this.w, this.h);
     this.renderer.setClearColor(this.bgColor);
+    this.mount.appendChild(this.renderer.domElement);
 
     this.scene = new THREE.Scene();
     this.camera = new THREE.PerspectiveCamera(45, this.w / this.h, 0.01, 100);
 
-    this.camera.position.z = 10;
+    this.camera.position.z = 5;
     this.mesh = new THREE.Mesh(
       new THREE.BoxGeometry(1, 1, 1),
       new THREE.MeshNormalMaterial()
     );
     this.scene.add(this.mesh);
-
     this.start();
+  }
+
+  update() {
+    this.mesh.rotation.x += 0.01;
+    this.mesh.rotation.y += 0.02;
+    this.renderer.render(this.scene, this.camera);
+    this.frameId = requestAnimationFrame(this.update);
   }
 
   start() {
@@ -51,18 +66,14 @@ class CanvasBackground extends React.Component {
     cancelAnimationFrame(this.frameId);
   }
 
-  update() {
-    this.mesh.rotation.x += 0.01;
-    this.mesh.rotation.y += 0.02;
-    this.renderer.render(this.scene, this.camera);
-    this.frameId = requestAnimationFrame(this.update);
-  }
-
   render() {
     return (
-      <div id="canvas-wrap">
-        <canvas id="canvas" />
-      </div>
+      <div
+        id="canvas-wrap"
+        ref={(mount) => {
+          this.mount = mount;
+        }}
+      />
     );
   }
 }
