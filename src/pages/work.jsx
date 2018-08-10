1647f6813479f5bf70f21564fbfe839f2a11f04d
Nop Jiarathanakul <prutsdomj@gmail.com>
2018-08-10 02:55:08 -0700

    style works page css

--- src/pages/work.jsx
+++ src/pages/work.jsx
@@ -7,7 +7,7 @@ export default ({ location, data }) => {
   const items = data.allWorksJson.edges.map(({ node }) => node);
 
   return (
-    <Layout location={location}>
+    <Layout location={location} width={900}>
       <h1>Work</h1>
       <h2>what {"I've"} been working on</h2>
       <WorkList items={items} />
