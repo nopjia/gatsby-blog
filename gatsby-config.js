16e483ad7ef9aa351b90ef5686265ca36fce310a
Nop Jiarathanakul <prutsdomj@gmail.com>
2018-08-30 23:53:09 -0700

    try gatsby-remark-images backgroundColor: transparent

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,8 @@ module.exports = {
           {
             resolve: "gatsby-remark-images",
             options: {
-              maxWidth: 590,
+              maxWidth: 600,
+              backgroundColor: "transparent",
             },
           },
           {
